01449a1c2e4b91186d3e21f0b79b283b3065b73b
Büşra Nur Saraçoğlu <91855028+BusraNurSaracoglu@users.noreply.github.com>
2022-04-29 16:48:48 +0300

    Add files via upload
    Bir dizeyi numpy kütüphanesi olmadan reverse edebilmek için yazılmış fonksiyon.

---
+++ reverse_array_without_numpy.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+c = []
+
+for i in reversed (range(n)):  
+    c.append(arr[i])
+
+for i in range(n):
+    print(c[i], end =" ")
